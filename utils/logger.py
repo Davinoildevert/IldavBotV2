79ac2cf528bec0ace0a2e3dd3d4a535f3892823d
Davinoildevert <davinoildevert10@gmail.com>
2026-09-19 12:27:36 +0200

    security: remove exposed credentials from logger comments

--- utils/logger.py
+++ utils/logger.py
@@ -2,7 +2,6 @@ import logging
 import os
 
 def setup_logger(log_file='logs/trading.log'):
-    # Crée le dossier logs si nécessaire
     os.makedirs(os.path.dirname(log_file), exist_ok=True)
     logging.basicConfig(
         level=logging.INFO,
@@ -23,10 +22,3 @@ def get_latency_logger(log_file='logs/latency_debug.log'):
         logger.addHandler(handler)
         logger.setLevel(logging.INFO)
     return logger
-
-#10006427957E-H7UsYj
-
-#davino  "telegram_api_id": "25848648", "telegram_api_hash": "0929af402e72f4cd99f8eb562932fff7",
-
-#antonio { "mt5_login": "93033025", "mt5_password": "4u_rWnHm","mt5_server": "MetaQuotes-Demo","telegram_api_id": "26031178","telegram_api_hash": "0823077004346c468b72c9ae9e6bc0f4",
-# " all_at_tp1 /progressive"
